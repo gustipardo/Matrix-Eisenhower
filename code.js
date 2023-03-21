fb50a51299e438103ad4b7eca005da31985e07b7
Gustavo Martin Alvarez <gusm2003@gmail.com>
2023-03-21 11:57:46 -0300

    Semi final product
    Movement inside lists working perfectly
    Automatic Save

--- code.js
+++ code.js
@@ -1,241 +1,193 @@
-let importantUrgent = [];
-let importantNoUrgent = [];
-let NoimportantUrgent = [];
-let NoimportantNoUrgent = [];
-let NoClasification = [];
 let valorSeleccionado;
-let elementosDiv;
 let cantidad = 0;
 let currentArray;
 
+const importantUrgent_list = document.getElementById("important-urgent__id");
+const importantNoUrgent_list = document.getElementById("important-not-urgent__id");
+const NoimportantUrgent_list = document.getElementById("not-important-urgent__id");
+const NoimportantNoUrgent_list = document.getElementById("not-important-not-urgent__id");
 
 
-
-function agregarString() {
-  elementosDiv = document.getElementById(valorSeleccionado); //Obtiene el id del div al que queremos agregarle mas elementos
+function agregarString() { //Agrega el string ingresado en el input en unos de los 5 arrays correspondiente.
   let userInput = document.getElementById("inputId").value; //toma el valor que escribio el usuario y lo guarda momentaneamente en userInput
-  const elementoDiv = document.createElement("div"); //Crea un elemento div y lo asigna a la variable elemento
   if (userInput !== "") { //Si lo que ingresa el usuario es diferente de nada pasará
-    currentArray.push(userInput); //Agrega el string del usuario al array correspondiente
+  let elementosDiv = document.getElementById(valorSeleccionado); //Obtiene el id del div al que queremos agregarle mas elementos
+  const elementoDiv = document.createElement("div"); //Crea un elemento div y lo asigna a la variable elemento
     elementoDiv.classList.add("element"); //Le asigna al div la clase "elemento"
-    elementoDiv.textContent = currentArray[currentArray.length - 1]; //Le agrega texto al div creado anteriormente con el ultimo elemento del array correspondiente
+    elementoDiv.setAttribute('data-id', userInput);
+    elementoDiv.textContent = userInput; //Le agrega texto al div creado anteriormente con el ultimo elemento del array correspondiente
     elementosDiv.appendChild(elementoDiv); // Agrega el nuevo Div creado dentro de el Div contenedor de una de las 4 secciones
-    console.log(importantUrgent,importantNoUrgent,NoimportantUrgent,NoimportantNoUrgent); //Muestra los arrays en consola
-    localStorage.setItem('importantUrgent', JSON.stringify(importantUrgent));
-    localStorage.setItem('importantNoUrgent', JSON.stringify(importantNoUrgent));
-    localStorage.setItem('NoimportantUrgent', JSON.stringify(NoimportantUrgent));
-    localStorage.setItem('NoimportantNoUrgent', JSON.stringify(NoimportantNoUrgent));
-
+    if (valorSeleccionado=="important-urgent__id") {
+      const IU = importantUrgent.toArray();
+      localStorage.setItem("IU", IU.join("-"));
+    } else if (valorSeleccionado=="important-not-urgent__id"){
+      const INU = importantNoUrgent.toArray();
+      localStorage.setItem("INU", INU.join("-"));
+    }
+    else if (valorSeleccionado=="not-important-urgent__id"){
+      const NIU = noImportantUrgent.toArray();
+      localStorage.setItem("NIU", NIU.join("-"));
+    }
+    else if (valorSeleccionado=="not-important-not-urgent__id"){
+      const NINU = noImportantNoUrgent.toArray();
+      localStorage.setItem("NINU", NINU.join("-"));
+    }
   }
 }
 
-document.addEventListener('DOMContentLoaded', function Recharge() {
-  if (JSON.parse(localStorage.getItem('importantUrgent')) !== null) {
-  importantUrgent = JSON.parse(localStorage.getItem('importantUrgent'));
-  }
-  if (JSON.parse(localStorage.getItem('importantNoUrgent')) !== null) {
-  importantNoUrgent = JSON.parse(localStorage.getItem('importantNoUrgent'));
-  }
-  if (JSON.parse(localStorage.getItem('NoimportantUrgent')) !== null) {
-  NoimportantUrgent = JSON.parse(localStorage.getItem('NoimportantUrgent'));
-  }
-  if (JSON.parse(localStorage.getItem('NoimportantNoUrgent')) !== null) {
-  NoimportantNoUrgent = JSON.parse(localStorage.getItem('NoimportantNoUrgent'));
-  }
 
 
-  for (var i = 0; i < importantUrgent.length; i++) {
-    let contenedorPadreIU = document.getElementById("important-urgent__id");
-    let contenedorHijoIU = document.createElement("div");
-    contenedorHijoIU.classList.add("element");
-    contenedorHijoIU.textContent = importantUrgent[i];
-    contenedorPadreIU.appendChild(contenedorHijoIU);
-  }
-  for (var i = 0; i < importantNoUrgent.length; i++) {
-    let contenedorPadreIU = document.getElementById("important-not-urgent__id");
-    let contenedorHijoIU = document.createElement("div");
-    contenedorHijoIU.classList.add("element");
-    contenedorHijoIU.textContent = importantNoUrgent[i];
-    contenedorPadreIU.appendChild(contenedorHijoIU);
-  }
-  for (var i = 0; i < NoimportantUrgent.length; i++) {
-    let contenedorPadreIU = document.getElementById("not-important-urgent__id");
-    let contenedorHijoIU = document.createElement("div");
-    contenedorHijoIU.classList.add("element");
-    contenedorHijoIU.textContent = NoimportantUrgent[i];
-    contenedorPadreIU.appendChild(contenedorHijoIU);
-  }
-  for (var i = 0; i < NoimportantNoUrgent.length; i++) {
-    let contenedorPadreIU = document.getElementById("not-important-not-urgent__id");
-    let contenedorHijoIU = document.createElement("div");
-    contenedorHijoIU.classList.add("element");
-    contenedorHijoIU.textContent = NoimportantNoUrgent[i];
-    contenedorPadreIU.appendChild(contenedorHijoIU);
-  } 
-
-  console.log(importantUrgent,importantNoUrgent,NoimportantUrgent,NoimportantNoUrgent);
-});
 
 
-function clase(){
+
+
+
+function clase(){ //Medianto lo seleccionado en el selector asigna un id y al current array correspondiente
   var select = document.getElementById("select");
   valorSeleccionado = select.value;
-  if (valorSeleccionado=="important-urgent__id") {
-    currentArray=importantUrgent;
-  } else if (valorSeleccionado=="important-not-urgent__id"){
-    currentArray=importantNoUrgent;
+}
+
+function reiniciarString(e) { //Limpia todos los string al igual que en el local storage.
+  if(!confirm('Are you sure?')) {
+    e.preventDefault();
   }
-  else if (valorSeleccionado=="not-important-urgent__id"){
-    currentArray=NoimportantUrgent;
+  localStorage.clear();
+
+  while (importantUrgent_list.children.length) {
+    importantUrgent_list.removeChild(importantUrgent_list.firstChild);
   }
-  else if (valorSeleccionado=="not-important-not-urgent__id"){
-    currentArray=NoimportantNoUrgent;
+
+  while (importantNoUrgent_list.children.length) {
+    importantNoUrgent_list.removeChild(importantNoUrgent_list.firstChild);
   }
-  else if (valorSeleccionado=="no-clasification"){
-    currentArray=NoClasification;
+
+  while (NoimportantUrgent_list.children.length) {
+    NoimportantUrgent_list.removeChild(NoimportantUrgent_list.firstChild);
+
+  }
+  while (NoimportantNoUrgent_list.children.length) {
+    NoimportantNoUrgent_list.removeChild(NoimportantNoUrgent_list.firstChild);
   }
 }
 
-function reiniciarString(e) {
-if(!confirm('Are you sure?')) {
-        e.preventDefault();
+ function activarBoton(event) { //Hace que el input se active con enter
+  if (event.keyCode === 13) {
+    agregarString();
+  }
+}
+
+
+
+let importantUrgent = new Sortable(importantUrgent_list, {
+    group: 'shared', // set both lists to same group
+    animation: 150,
+    dragClass: "sortable-drag",
+    filter: '.filtered',
+    store: {
+      set: (sortable) => {
+        const IU = sortable.toArray();
+        localStorage.setItem("IU", IU.join("-"));
+      },
+      get: (sortable) => {
+        const conjunto = localStorage.getItem("IU");
+
+        if (conjunto !== null) {
+          const array = conjunto.split('-');
+          for (var i = 0; i < array.length; i++) {
+            let contenedorPadreIU = document.getElementById("important-urgent__id");
+            let contenedorHijoIU = document.createElement("div");
+            contenedorHijoIU.classList.add("element");
+            contenedorHijoIU.setAttribute('data-id', array[i]);
+            contenedorHijoIU.textContent = array[i];
+            contenedorPadreIU.appendChild(contenedorHijoIU);}
+          } 
+        },
+        onDeselect: (evt) => {
+          console.log('onChoose');
+        },
+        
+      }
+    });
+
+let importantNoUrgent = new Sortable(importantNoUrgent_list, {
+  group: 'shared',
+  animation: 150,
+  dragClass: "sortable-drag",
+  filter: '.filtered',
+  store: {
+    set: (sortable) => {
+      const INU = sortable.toArray();
+      localStorage.setItem("INU", INU.join("-"));
+    },
+    get: (sortable) => {
+      const conjunto = localStorage.getItem("INU");
+      if (conjunto !== null) {
+        const array = conjunto.split('-');
+        for (var i = 0; i < array.length; i++) {
+          let contenedorPadreIU = document.getElementById("important-not-urgent__id");
+          let contenedorHijoIU = document.createElement("div");
+          contenedorHijoIU.classList.add("element");
+          contenedorHijoIU.setAttribute('data-id', array[i]);
+          contenedorHijoIU.textContent = array[i];
+          contenedorPadreIU.appendChild(contenedorHijoIU);} 
+        } 
+      } 
     }
 
-let importantUrgent = [];
-let importantNoUrgent = [];
-let NoimportantUrgent = [];
-let NoimportantNoUrgent = [];
-let contenedorImportantUrgent = document.getElementById("important-urgent__id");
-let contenedorImportantNoUrgent = document.getElementById("important-not-urgent__id");
-let contenedorNoimportantUrgent = document.getElementById("not-important-urgent__id");
-let contenedorNoimportantNoUrgent = document.getElementById("not-important-not-urgent__id");
-localStorage.setItem('importantUrgent', JSON.stringify(importantUrgent));
-localStorage.setItem('importantNoUrgent', JSON.stringify(importantNoUrgent));
-localStorage.setItem('NoimportantUrgent', JSON.stringify(NoimportantUrgent));
-localStorage.setItem('NoimportantNoUrgent', JSON.stringify(NoimportantNoUrgent));
+  });
 
-while (contenedorImportantUrgent.children.length > 1) {
-    contenedorImportantUrgent.removeChild(contenedorImportantUrgent.children[1]);
-  }
+let noImportantUrgent = new Sortable(NoimportantUrgent_list, {
+    group: 'shared', // set both lists to same group
+    animation: 150,
+    dragClass: "sortable-drag",
+    filter: '.filtered',
+    store: {
+      set: (sortable) => {
+        const NIU = sortable.toArray();
+        localStorage.setItem("NIU", NIU.join("-"));
+      },
+      get: (sortable) => {
+       const conjunto = localStorage.getItem("NIU");
+       if (conjunto !== null) {
+         const array = conjunto.split('-');
+         for (var i = 0; i < array.length; i++) {
+          let contenedorPadreIU = document.getElementById("not-important-urgent__id");
+          let contenedorHijoIU = document.createElement("div");
+          contenedorHijoIU.classList.add("element");
+          contenedorHijoIU.setAttribute('data-id', array[i]);
+          contenedorHijoIU.textContent = array[i];
+          contenedorPadreIU.appendChild(contenedorHijoIU);}
+        } 
+      } 
+    }
 
-while (contenedorImportantNoUrgent.children.length > 1) {
-    contenedorImportantNoUrgent.removeChild(contenedorImportantNoUrgent.children[1]);
-  }
+  });
 
-  while (contenedorNoimportantUrgent.children.length > 1) {
-    contenedorNoimportantUrgent.removeChild(contenedorNoimportantUrgent.children[1]);
-
-  }
-  while (contenedorNoimportantNoUrgent.children.length > 1) {
-    contenedorNoimportantNoUrgent.removeChild(contenedorNoimportantNoUrgent.children[1]);
-  }
-
-
-
-}
-
- function activarBoton(event) {
-        if (event.keyCode === 13) {
-          agregarString();
-        }
-      }
-
-
-
-
-
-
-
-
-
-
-
-dragElement(document.getElementById("no-clasification"));
-
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
+let noImportantNoUrgent = new Sortable(NoimportantNoUrgent_list, {
+  group: 'shared',
+  animation: 150,
+  dragClass: "sortable-drag",
+  filter: '.filtered',
+  store: {
+    set: (sortable) => {
+      const NINU = sortable.toArray();
+      localStorage.setItem("NINU", NINU.join("-"));
+    },
+    get: (sortable) => {
+      const conjunto = localStorage.getItem("NINU");
+      if (conjunto !== null) {
+        const array = conjunto.split('-');
+        for (var i = 0; i < array.length; i++) {
+          let contenedorPadreIU = document.getElementById("not-important-not-urgent__id");
+          let contenedorHijoIU = document.createElement("div");
+          contenedorHijoIU.classList.add("element");
+          contenedorHijoIU.setAttribute('data-id', array[i]);
+          contenedorHijoIU.textContent = array[i];
+          contenedorPadreIU.appendChild(contenedorHijoIU);}
+        } 
+      } 
+    }
+  });
 
 
 
-
-// Make the DIV element draggable:
-dragElement(document.getElementById("no-clasification"));
-
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
